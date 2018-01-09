313fd16303e20d8baf320b986cb825ad1a337bfb
César Guadarrama Cantú <cesarguadarrama@icloud.com>
2018-01-08 22:22:01 -0600

    Added perimeter

--- src/js/containers/Perimeter.js
+++ src/js/containers/Perimeter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
+import { Table } from '../components'
 import { } from '../actions'
 
 class Perimeter extends Component {
@@ -9,8 +10,20 @@ class Perimeter extends Component {
     super(props)
 
     this.state = {
-
+      logs: [0,0,0,0,0,0],
+      alerts: [],
+      selectedElementIndex: [null, null],
+      showLogDetail: false
     }
+
+    this.onLogSelect = this.onLogSelect.bind(this)
+  }
+
+  onLogSelect(item) {
+    this.setState({
+      showLogDetail: true,
+      selectedLog: item
+    })
   }
 
   render() {
@@ -20,6 +33,60 @@ class Perimeter extends Component {
       <div className="app-content perimeter small-padding">
         <div className="content">
           <h2>Perímetro</h2>
+          <Table
+            actionsContainer={
+              <div>
+                <p className="button action">Enero 3 - Hoy</p>
+                <p className="button action">Filtrar</p>
+              </div>
+            }
+            selectedElementIndex={state.selectedElementIndex}
+            element={(item, index, sectionIndex) =>
+              <div className={`table-item ${state.selectedElementIndex[0] === index && state.selectedElementIndex[1] === sectionIndex ? 'selected' : ''}`}
+                key={index}
+                onClick={() => this.onLogSelect(item, index, sectionIndex)}>
+                <div className="medium">3 enero <span>7:45 AM</span></div>
+                <div className="medium">Camioneta</div>
+                <div>50</div>
+                <div>1 Norte</div>
+                <div className="medium">Andrés López</div>
+                <div>Proveedor</div>
+              </div>
+            }
+            elements={[
+              { title: 'Registros', elements: state.logs},
+              { title: 'Alertas', elements: state.alerts}
+            ]}
+            titles={[
+              {title: 'Tiempo', className: 'medium'},
+              {title: 'Tipo de vehículo', className: 'medium'},
+              {title: 'Sitio'},
+              {title: 'Acceso'},
+              {title: 'Persona autorizada', className: 'medium'},
+              {title: 'Tipo de acceso'}
+            ]}
+          />
+        </div>
+        <div className={`log-detail-container ${state.showLogDetail ? '' : 'hidden'}`}>
+          <div className="content">
+            <span onClick={() => this.setState({ showLogDetail: false, selectedElementIndex: [null,null] })} className="close">Cerrar</span>
+            <div className="time-location">
+              <p>3 Enero 07:45 PM</p>
+              <p>Zona <span>Norte</span> Sitio <span>5</span></p>
+            </div>
+            <div className="image-slider">
+
+            </div>
+            <div className="detail">
+              <span>Galería</span>
+              <div className="image-slider">
+
+              </div>
+            </div>
+            <div className="action destructive">
+              <p>Contactar seguridad</p>
+            </div>
+          </div>
         </div>
       </div>
     )
