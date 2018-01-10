d177908b34b762511e40c322e5e04ef7405021b2
César Guadarrama Cantú <cesarguadarrama@icloud.com>
2018-01-09 19:00:08 -0600

    Added facial recognition

--- src/js/containers/Cctv.js
+++ src/js/containers/Cctv.js
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 
 import { } from '../actions'
+import { Table } from '../components'
 
 class Cctv extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-
+      logs: [0,0,0,0,0,0],
+      alerts: []
     }
   }
 
@@ -24,6 +26,60 @@ class Cctv extends Component {
         </Helmet>
         <div className="content">
           <h2>CCTV</h2>
+          <Table
+            actionsContainer={
+              <div>
+                <p className="button action">Enero 3 - Hoy</p>
+                <p className="button action">Filtrar</p>
+              </div>
+            }
+            selectedElementIndex={state.selectedElementIndex}
+            element={(item, index, sectionIndex) =>
+              <div className={`table-item ${state.selectedElementIndex[0] === index && state.selectedElementIndex[1] === sectionIndex ? 'selected' : ''}`}
+                key={index}
+                onClick={() => this.onLogSelect(item, index, sectionIndex)}>
+                <div className="medium">3 enero <span>7:45 AM</span></div>
+                <div className="medium">Camioneta</div>
+                <div>50</div>
+                <div>1 Norte</div>
+                <div className="medium">Andrés López</div>
+                <div>Proveedor</div>
+              </div>
+            }
+            elements={[
+              { title: 'Registros', elements: state.logs},
+              { title: 'Alertas', elements: state.alerts}
+            ]}
+            titles={[
+              {title: 'Tiempo', className: 'medium'},
+              {title: 'Tipo de vehículo', className: 'medium'},
+              {title: 'Sitio'},
+              {title: 'Acceso'},
+              {title: 'Persona autorizada', className: 'medium'},
+              {title: 'Tipo de acceso'}
+            ]}
+          />
+        </div>
+        <div className={`log-detail-container ${state.showLogDetail ? '' : 'hidden'}`}>
+          <div className="content">
+            <span onClick={() => this.setState({ showLogDetail: false, selectedElementIndex: [null,null] })} className="close">Cerrar</span>
+            <div className="time-location">
+              <p>3 Enero 07:45 PM</p>
+              <p>Zona <span>Norte</span> Sitio <span>5</span></p>
+            </div>
+            <div className="image-slider">
+
+            </div>
+            <div className="detail">
+              <span>Galería</span>
+              <div className="image-slider">
+
+              </div>
+            </div>
+            <div className="action destructive">
+              <p>Contactar seguridad</p>
+            </div>
+          </div>
         </div>
       </div>
     )
--- src/js/containers/FacialRecognition.js
+++ src/js/containers/FacialRecognition.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 
+import { Table } from '../components'
 import { } from '../actions'
 
 class FacialRecognition extends Component {
@@ -10,8 +11,19 @@ class FacialRecognition extends Component {
     super(props)
 
     this.state = {
-
+      logs: [0,0,0,0,0,0,0],
+      selectedElementIndex: [null,null],
+      showLogDetail: false
     }
+
+    this.onLogSelect = this.onLogSelect.bind(this)
+  }
+
+  onLogSelect(item) {
+    this.setState({
+      showLogDetail: true,
+      selectedLog: item
+    })
   }
 
   render() {
@@ -24,6 +36,87 @@ class FacialRecognition extends Component {
         </Helmet>
         <div className="content">
           <h2>Reconocimiento Facial</h2>
+          <Table
+            actionsContainer={
+              <div>
+                <p className="button action">Enero 3 - Hoy</p>
+                <p className="button action">Filtrar</p>
+              </div>
+            }
+            selectedElementIndex={state.selectedElementIndex}
+            element={(item, index, sectionIndex) =>
+              <div className={`table-item ${state.selectedElementIndex[0] === index && state.selectedElementIndex[1] === sectionIndex ? 'selected' : ''}`}
+                key={index}
+                onClick={() => this.onLogSelect(item, index, sectionIndex)}>
+                <div className="medium">3 enero <span>7:45 AM</span></div>
+                <div className="medium">Camioneta</div>
+                <div>50</div>
+                <div>1 Norte</div>
+                <div className="medium">Andrés López</div>
+                <div>Proveedor</div>
+              </div>
+            }
+            elements={[
+              { title: 'Registros', elements: state.logs},
+              { title: 'Alertas', elements: state.alerts}
+            ]}
+            titles={[
+              {title: 'Tiempo', className: 'medium'},
+              {title: 'Tipo de vehículo', className: 'medium'},
+              {title: 'Sitio'},
+              {title: 'Acceso'},
+              {title: 'Persona autorizada', className: 'medium'},
+              {title: 'Tipo de acceso'}
+            ]}
+          />
+          <div className={`log-detail-container ${state.showLogDetail ? '' : 'hidden'}`}>
+            <div className="content">
+              <span onClick={() => this.setState({ showLogDetail: false, selectedElementIndex: [null,null] })} className="close">Cerrar</span>
+              <div className="time-location">
+                <p>3 Enero 07:45 PM</p>
+                <p>Zona <span>Norte</span> Sitio <span>5</span></p>
+              </div>
+              <div className="detail">
+                <span>Rostro detectado</span>
+                <div className="image-slider">
+
+                </div>
+              </div>
+              <div className="details-container">
+                <div className="detail">
+                  <span>Hora del suceso</span>
+                  <p>Kia</p>
+                </div>
+                <div className="detail">
+                  <span>Tipo de ingreso</span>
+                  <p>Kia</p>
+                </div>
+                <div className="detail">
+                  <span>Método de ingreso</span>
+                  <p>Kia</p>
+                </div>
+                <div className="detail">
+                  <span>Coincide con registro</span>
+                  <p>Si</p>
+                </div>
+                <div className="detail">
+                  <span>Estatus</span>
+                  <p>Continua dentro</p>
+                </div>
+                <div className="detail">
+                  <span>Huella dactilar</span>
+                  <img src="" alt=""/>
+                </div>
+                <div className="detail">
+                  <span>Código único de identificación</span>
+                  <img src="" alt=""/>
+                </div>
+              </div>
+              <div className="action destructive">
+                <p>Contactar seguridad</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
