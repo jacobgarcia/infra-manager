b3c63665fb2f6ef698ec4c9f6cadd3a6e99fbcc2
César Guadarrama Cantú <cesarguadarrama@icloud.com>
2018-01-21 13:14:36 -0600

    Added detail view

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -2,16 +2,29 @@ import React from 'react'
 
 function Card(props) {
   return (
-    <div className={`card ${props.className}`}>
-      {
-        props.title
-        &&
-        <div className="title">
-          { props.title }
-        </div>
-      }
-      <div className="content">
-        { props.children }
+    <div className={`card ${props.className} ${props.full ? 'full' : ''}`}>
+      <div className="card-header">
+        {
+          props.title
+          &&
+          <div className="title">
+            { props.title }
+          </div>
+        }
+        {
+          props.detailActions && props.full
+          &&
+          <div className="actions">
+            { props.detailActions }
+          </div>
+        }
+      </div>
+      <div className="card-content">
+        {
+          props.full
+          ? props.detailView
+          : props.children
+        }
       </div>
     </div>
   )
