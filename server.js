fb83a2884d74c9a6feafb35749074b121d15e526
Jacob Garcia <jacob@Sacrifice.local>
2018-03-23 16:09:35 -0600

    Removed Node Media Server

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ const app = express()
 
 const v1 = require(path.resolve('router/v1'))
 
-// RTMP Server
-const NodeMediaServer = require('node-media-server')
-
 const PORT = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({ limit: '12mb' }))
@@ -107,21 +104,3 @@ io.on('connection', socket => {
 })
 
 global.io = io
-
-// Start RTMP Server
-const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 60,
-    ping_timeout: 30
-  },
-  http: {
-    port: 8000,
-    allow_origin: '*'
-  }
-}
-
-const nms = new NodeMediaServer(config)
-nms.run()
