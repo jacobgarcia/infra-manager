07cf26c9236f0ed5fd894abc9341f4f6ea2e1075
Jacob Garcia <jacob@Sacrifice.local>
2018-03-26 18:17:51 -0600

    Logging again

--- router/v1/companies.js
+++ router/v1/companies.js
@@ -269,6 +269,7 @@ router.route('/sites/initialize')
   // Since is not human to check which company ObjectId wants to be used, a search based on the name is done
   const { id, version, company, key, name, position, country } = req.body
   let { sensors, cameras } = req.body
+  console.log(sensors)
   sensors = JSON.parse(sensors)
   cameras = JSON.parse(cameras)
   if (!id || !version || !company || !key || !name || !position || !sensors || !cameras || !country) return res.status(400).json({ success: false, message: 'Malformed request'})
