7ec8ddb9b1df5b73ade399b3222c98f492120573
Jacob Garcia <jacob@Sacrifice.local>
2018-04-02 17:43:50 -0500

    Fixed error in commit

--- src/js/containers/VideoSurveillance.js
+++ src/js/containers/VideoSurveillance.js
@@ -107,7 +107,7 @@ class VideoSurveillance extends Component {
                     {
                       state.playingVideo
                       ?
-                      <ReactPlayer url={'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'} playing width="340" height="240" controls fileConfig={{ attributes: { poster: state.selectedLog.photo } }}/>
+                      <ReactPlayer url={'https://stream.connus.mx/hls/' + state.room + '.m3u8'} playing width="340" height="240" controls fileConfig={{ attributes: { poster: state.selectedLog.photo } }}/>
                       : null
                     }
                 </div>
