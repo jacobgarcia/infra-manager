0a7bd2cb19df9c8d633f7c6485b7fe99418704a3
JacobGarcia <rock2494@hotmail.com>
2018-04-16 19:22:27 -0500

    Bearer token

--- router/v1/auth.js
+++ router/v1/auth.js
@@ -194,27 +194,27 @@ router.route('/authenticate').post((req, res) => {
     })
 })
 
-// router.use((req, res, next) => {
-//   const bearer = req.headers.authorization || 'Bearer '
-//   const token = bearer.split(' ')[1]
-//
-//   if (!token) {
-//     return res
-//       .status(401)
-//       .send({ error: { message: 'No bearer token provided' } })
-//   }
-//
-//   return jwt.verify(token, config.secret, (err, decoded) => {
-//     if (err) {
-//       winston.error('Failed to authenticate token', err, token)
-//       return res
-//         .status(401)
-//         .json({ error: { message: 'Failed to authenticate  bearer token' } })
-//     }
-//
-//     req._user = decoded
-//     return next()
-//   })
-// })
+router.use((req, res, next) => {
+  const bearer = req.headers.authorization || 'Bearer '
+  const token = bearer.split(' ')[1]
+
+  if (!token) {
+    return res
+      .status(401)
+      .send({ error: { message: 'No bearer token provided' } })
+  }
+
+  return jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      winston.error('Failed to authenticate token', err, token)
+      return res
+        .status(401)
+        .json({ error: { message: 'Failed to authenticate  bearer token' } })
+    }
+
+    req._user = decoded
+    return next()
+  })
+})
 
 module.exports = router
