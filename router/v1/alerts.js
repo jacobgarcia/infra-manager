fe4ea638803f35baaae8d5df4ef69c086869ad7a
Jacob García <rock2494@hotmail.com>
2018-06-08 12:55:13 -0500

    Added company to Site.findOne

--- router/v1/alerts.js
+++ router/v1/alerts.js
@@ -7,6 +7,7 @@ const request = require('request')
 
 const Alert = require(path.resolve('models/Alert'))
 const Site = require(path.resolve('models/Site'))
+const Company = require(path.resolve('models/Company'))
 
 /** *** ALERTS ****/
 // PATCH: This method will be DEPRECATED but since ATT FRMs are using this, until we update them we'll be using this patch
@@ -17,75 +18,87 @@ router.route('/alerts').post((req, res) => {
   // Emit alert socket
   global.io.to('connus').emit('alert', data)
 
-  // First, find the sensor for the specified KEY
-  Site.findOne({ key: site }).exec((error, currentSite) => {
+  Company.findOne({ name: 'AT&T' }).exec((error, company) => {
     if (error) {
       winston.error(error)
-      return res
-        .status(500)
-        .json({ success: false, message: 'Could not update sensors' })
+      return res.status(500).json({ error })
     }
-    if (!currentSite) return res.status(404).json({ success: false, message: 'Site not found' })
+    if (!company) return res
+        .status(404)
+        .json({ success: false, message: 'Specified company was not found' })
 
-    // Since we have shitty name for sensors we have to transform it into the new way of doing it
-    let key,
-      type = null
-    switch (alert) {
-      case 'contactsensor1':
-        key = 1
-        type = 'contact'
-        break
-      case 'contactsensor2':
-        key = 2
-        type = 'contact'
-        break
-      case 'vibrationsensor1':
-        key = 1
-        type = 'vibration'
-        break
-      case 'vibrationsensor2':
-        key = 2
-        type = 'vibration'
-        break
-      default:
-    }
-
-    // Find which element must need to be modified
-    const sensor = currentSite.sensors.find(
-      $0 => $0.key === key && $0.class === type
-    )
-
-    console.log(sensor, key, type, currentSite)
-
-    // Set sensor value to 0. All 0's are bad in thos context
-    if (sensor) sensor.value = 0
-    const body = {
-      key: site,
-      company: 'AT&T',
-      sensors: currentSite.sensors
-    }
-
-    request.put(
-      {
-        url: 'https://api.connus.mx/v1/sites/sensors',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + req._token,
-          body
-        }
-      },
-      error => {
-        if (error) {
-          winston.error(error)
-          return res
-            .status(500)
-            .json({ success: false, message: 'Could not update sensors' })
-        }
+    // First, find the sensor for the specified KEY
+    Site.findOne({ company, key: site }).exec((error, currentSite) => {
+      if (error) {
+        winston.error(error)
         return res
-          .status(200)
-          .json({ success: true, message: 'Sent alert to client' })
+          .status(500)
+          .json({ success: false, message: 'Could not update sensors' })
       }
-    )
+      if (!currentSite) return res
+          .status(404)
+          .json({ success: false, message: 'Site not found' })
+
+      // Since we have shitty name for sensors we have to transform it into the new way of doing it
+      let key,
+        type = null
+      switch (alert) {
+        case 'contactsensor1':
+          key = 1
+          type = 'contact'
+          break
+        case 'contactsensor2':
+          key = 2
+          type = 'contact'
+          break
+        case 'vibrationsensor1':
+          key = 1
+          type = 'vibration'
+          break
+        case 'vibrationsensor2':
+          key = 2
+          type = 'vibration'
+          break
+        default:
+      }
+
+      // Find which element must need to be modified
+      const sensor = currentSite.sensors.find(
+        $0 => $0.key === key && $0.class === type
+      )
+
+      console.log(sensor, key, type, currentSite)
+
+      // Set sensor value to 0. All 0's are bad in thos context
+      if (sensor) sensor.value = 0
+      const body = {
+        key: site,
+        company: 'AT&T',
+        sensors: currentSite.sensors
+      }
+
+      request.put(
+        {
+          url: 'https://api.connus.mx/v1/sites/sensors',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + req._token,
+            body
+          }
+        },
+        error => {
+          if (error) {
+            winston.error(error)
+            return res
+              .status(500)
+              .json({ success: false, message: 'Could not update sensors' })
+          }
+          return res
+            .status(200)
+            .json({ success: true, message: 'Sent alert to client' })
+        }
+      )
+    })
   })
 })
 
