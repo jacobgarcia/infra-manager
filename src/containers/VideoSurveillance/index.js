84b7a09bfba76f433e8662b6f2b744a50d2b659f
Unknown <franscaraveli@gmail.com>
2018-06-21 13:03:16 -0500

    Adding IPConfigure

--- src/containers/VideoSurveillance/index.js
+++ src/containers/VideoSurveillance/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
+import { NetworkOperationVMS } from 'lib'
+
+let container = null
 
 class VideoSurveillance extends Component {
   constructor(props) {
@@ -10,21 +13,21 @@ class VideoSurveillance extends Component {
 
   componentDidMount() {
     let frame1 = document.createElement('iframe')
-    frame1.src = "http://192.168.100.119:8080/?single-player=27"
-    frame1.orchidId = "0ab98ec0-753a-4596-b8d1-74d68787b2f7"
     frame1.setAttribute('onload', this.init(frame1))
+    frame1.src = "http://25463c71.ngrok.io/?single-player=1"
+    frame1.orchidId = "fdfacc2f-4c42-4484-bbb1-9ba7dd4372fc"
     console.log("src defining")
-    let container = document.getElementById('players')
-    container.appendChild(frame1)
+    container = document.getElementById('players')
+    console.log("apending C")
   }
 
   init(frame) {
     this.getFusionToken(token => {
-        frame.authToken = token
+        container.appendChild(frame)
         this.renewJWT(frame)
         window.setInterval(() => {
           this.renewJWT(frame)
-        }, 60000)
+        }, 30000)
       }
     )
   }
@@ -32,38 +35,36 @@ class VideoSurveillance extends Component {
   setJWT(frame, jwt) {
     console.log(jwt)
     frame.contentWindow.postMessage({'jwt': jwt}, '*')
+    console.log("puse el jwt")
   }
 
   renewJWT(frame) {
-    let xhttp = new XMLHttpRequest()
-    xhttp.open("GET", "http://192.168.100.119:8080/fusion/tokens?type=orchid", true)
-    xhttp.setRequestHeader("Authorization", "Bearer " + frame.authToken)
-    xhttp.onreadystatechange = () => {
-      if (xhttp.readyState === 4 && xhttp.status === 200) {
-      // Success-- retrieve our JWT from the response
-      console.log("JWT obtained")
-      let jwt = JSON.parse(xhttp.responseText)[frame.orchidId]
-      this.setJWT(frame, jwt)
-      }
-    }
-    xhttp.send()
+    NetworkOperationVMS.getJWT()
+      .then(({data}) => {
+        console.log("JWT obtained")
+        this.setJWT(frame, data[frame.orchidId])
+      })
+      .catch(({response = {} }) => {
+        const { status = 500 } = response
+        console.log(status)
+      })
   }
+
   getFusionToken(callback) {
     let data = JSON.stringify({
       "username": "admin",
       "password": "admin"
     })
-    let xhttp = new XMLHttpRequest()
-    xhttp.withCredentials = true
-    xhttp.open("POST", "http://192.168.100.119:8080/fusion/users/login")
-    xhttp.setRequestHeader("content-type", "application/json")
-    xhttp.onreadystatechange = () => {
-      if (xhttp.readyState === 4 && xhttp.status === 200) {
+    NetworkOperationVMS.login(data)
+      .then(({data}) => {
+        localStorage.setItem('vmstoken', data.token)
         console.log("Token obtaind")
-        callback(JSON.parse(xhttp.responseText).token)
-      }
-    }
-    xhttp.send(data)
+        callback(data.token)
+      })
+      .catch(({response = {} }) => {
+        const { status = 500 } = response
+        console.log(status)
+      })
   }
 
   render() {
