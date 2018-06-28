5b206186e99a1e9d3c55eadb4099584157f78ab9
Jacob García <rock2494@hotmail.com>
2018-06-28 12:27:48 -0500

    Added id to alarms

--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 class Alerts extends Component {
   constructor(props) {
@@ -13,7 +12,7 @@ class Alerts extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     if (this.props.alerts.length === nextProps.alerts.length) return
 
     const today = new Date().getDay()
@@ -23,7 +22,7 @@ class Alerts extends Component {
     }
 
     this.props.alerts.map(alert => {
-      alert.alarms.map(({ timestamp, values }) => {
+      alert.alarms.map(({ timestamp }) => {
         if (new Date(timestamp).getDay() === today) {
           alerts.today = alerts.today.concat([{ timestamp, ...alert }])
         } else {
@@ -46,9 +45,6 @@ class Alerts extends Component {
             className={`tooltip ${props.isCreating && 'hidden'}`}
             onClick={props.onVisibleToggle}
           />
-          {/* <div className="mini-header">
-            <span className="pop-window">Hacer ventana</span>
-          </div> */}
           <div>
             {state.alerts.today.map((alert, index) => (
               <p key={index}>{JSON.stringify(alert.timestamp)}</p>
@@ -75,52 +71,8 @@ Alerts.defaultProps = {
   alerts: []
 }
 
-export default Alerts
+Alerts.propTypes = {
+  alerts: []
+}
 
-// {
-//   "site": {
-//     "_id": "5a0b805dd6e4539441f79601",
-//     "key": "23dTas2"
-//   },
-//   "zone": {
-//     "_id": "5a0b3d43d357508658895c91",
-//     "name": "Centro"
-//   },
-//   "subzone": {
-//     "_id": "5a0b3d55d357508658895c92",
-//     "name": "Valle de México"
-//   },
-//   "alarms": [
-//     {
-//       "timestamp": 1511253451562,
-//       "values": [
-//         {
-//           "key": "ts3",
-//           "value": 90,
-//           "_id": "5a13e5cbfd09dd3b94505b92"
-//         }
-//       ],
-//       "attended": false
-//     }
-//   ],
-//   "sensors": [
-//     {
-//       "timestamp": 1511253451562,
-//       "values": [
-//         {
-//           "key": "ts1",
-//           "value": 1.8,
-//           "_id": "5a13e5cbfd09dd3b94505b91"
-//         }, {
-//           "key": "ts2",
-//           "value": 41.8,
-//           "_id": "5a13e5cbfd09dd3b94505b90"
-//         }, {
-//           "key": "ts3",
-//           "value": 60.8,
-//           "_id": "5a13e5cbfd09dd3b94505b8f"
-//         }
-//       ]
-//     }
-//   ]
-// }
+export default Alerts
