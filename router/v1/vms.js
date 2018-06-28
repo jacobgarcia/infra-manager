b2e88729813e09a6cefff639b97db8be20446799
frans06 <franscaraveli@gmail.com>
2018-06-28 18:31:35 -0500

    merge

--- router/v1/vms.js
+++ router/v1/vms.js
@@ -7,8 +7,12 @@ const jwt = require('jsonwebtoken')
 const base64url = require('base64url')
 
 router.route('/tokens').get((req,res) => {
-  var token = jwt.sign({
-  "exp": Math.floor(Date.now() / 1000) + (60 * 60),
-  "iat": Date.now()
-},base64url('mefcwbUTxYZHLa_EalRisajyFZD8dCLHYkcBQ1mWuiA'))
+  const { core, user, pass, streamid} = req.body
+  const token = jwt.sign({
+    "exp": Math.floor(Date.now() / 1000) + (60 * 60),
+    "iat": Date.now()
+  },base64url('mefcwbUTxYZHLa_EalRisajyFZD8dCLHYkcBQ1mWuiA'))
+
 })
+
+module.exports = router
