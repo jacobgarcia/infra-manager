77efc5c0daab2d2076a15ffcf58ba36420cc9bcf
frans06 <franscaraveli@gmail.com>
2018-06-29 20:59:44 -0500

    last update

--- models/Stream.js
+++ models/Stream.js
@@ -3,13 +3,20 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const schema = new Schema({
-  id: { type: String, unique: true },
+  id: String,
+  core: { type: String, unique: true },
+  user: String,
+  pass: String,
+  streamid: String,
   name: String,
+  version: String,
+  country: String,
+  zone: String,
   room: { type: String },
   company: { type: Schema.Types.ObjectId, ref: 'Company', required: true },
   site: { type: Schema.Types.ObjectId, ref: 'Site' },
-  records: [String],
-  photo: String
+  photo: String,
+  issuerkey: String,
 })
 
 module.exports = mongoose.model('Stream', schema)
--- router/v1/vms.js
+++ router/v1/vms.js
@@ -5,14 +5,197 @@ const router = new express.Router()
 const request = require('request')
 const jwt = require('jsonwebtoken')
 const base64url = require('base64url')
+const ObjectID = mongo.ObjectID
+const crypto = require('crypto')
+const uuid = require('uuid/v1')
+const Zone = require(path.resolve('models/Zone'))
+const Site = require(path.resolve('models/Site'))
+const Company = require(path.resolve('models/Company'))
+const Stream = require(path.resolve('models/Stream'))
 
-router.route('/tokens').get((req,res) => {
-  const { core, user, pass, streamid} = req.body
-  const token = jwt.sign({
-    "exp": Math.floor(Date.now() / 1000) + (60 * 60),
-    "iat": Date.now()
-  },base64url('mefcwbUTxYZHLa_EalRisajyFZD8dCLHYkcBQ1mWuiA'))
+router.route('/stream').post((req,res) => {
+  let { core, user, pass, streamid} = req.body
+  const { version, company, key, name, country, zone } = req.body
 
+  if (
+    !core ||
+    !user ||
+    !pass ||
+    !streamid ||
+    !version ||
+    !company ||
+    !key ||
+    !name ||
+    !country ||
+    !zone
+  ) return res
+    .status(400)
+    .json({ success: false, message: 'Malformed request' })
+  return Company.findOne({ name: company }).exec((error, company) => {
+    if (error) {
+      winston.error(error)
+      return res.status(500).json({ error })
+    }
+    if (!company) return res
+        .status(404)
+        .json({ success: false, message: 'Specified company was not found' })
+    return Zone.findOne({ name: zone, company: company._id }).exec(
+      (error, zone) => {
+        if (error) {
+          winston.error(error)
+          return res.status(500).json({ error })
+        }
+        if (!zone) return res
+            .status(404)
+            .json({ success: false, message: 'Specified zone was not found' })
+        return Site.findOne({ key, company: company._id }).exec(
+          (error, site) => {
+            if (error) {
+              winston.error(error)
+              return res.status(500).json({
+                success: false,
+                message:
+                  'Could not find site',
+                error
+              })
+            }
+            request.get("http://192.168.1.48/service/trusted/issuer?version=2", {
+              'auth': {
+                'user': 'admin',
+                'pass': 'admin'
+              }
+            }, (err, res, body) => {
+                if (res.statusCode == 200) {
+                  return Stream.findOne({ body.id , company: company._id}).exec(
+                    (error, stream) => {
+                      !stream ? {
+                        let issuerkey = { issuerkey: stream.issuerkey }
+                        let id = { id: stream.id }
+                        return res.status(200).json({ site })
+                      } : {
+                        return res.status(500).json({
+                          success: false,
+                          message:
+                            'Issuer ocupated, delete first',
+                          error
+                          })
+                      }
+                    })
+                } else {
+                  let id = { id: uuid() }
+                  let issuerkey = { issuerkey: base64url(crypto.randomBytes(32)) }
+                  request({
+                    url: `http://192.168.1.48/service/trusted/issuer?version=2`,
+                    method: "POST",
+                    headers: {
+                        "content-type": "application/json",
+                        },
+                    json: {
+                    "id": id.id,
+                    "access_token": "",
+                    "key": {
+                      "kty": "oct",
+                      "k": issuerkey.issuerkey
+                    },
+                    "description": "",
+                    "uri": ""
+                    }
+                  },
+                  {
+                  'auth': {
+                    'user': 'admin',
+                    'pass': 'admin'
+                    }
+                  },
+                  (err, res, body) => {
+                  })
+                  return res.status(200).json({ site })
+                }            })
+            new Stream({
+              id,
+              core,
+              user,
+              pass,
+              streamid,
+              version,
+              company,
+              site,
+              name,
+              country,
+              issuerkey,
+              zone
+            }).save()
+            return res.status(200).json({ site })
+          })
+      })
+  })
+})
+.get((req,res) => {
+  let { core, user, pass, streamid} = req.body
+  const { version, company, key, name, country, zone } = req.body
+
+  if (
+    !core ||
+    !user ||
+    !pass ||
+    !streamid ||
+    !version ||
+    !company ||
+    !key ||
+    !name ||
+    !country ||
+    !zone
+  ) return res
+    .status(400)
+    .json({ success: false, message: 'Malformed request' })
+  return Company.findOne({ name: company }).exec((error, company) => {
+    if (error) {
+      winston.error(error)
+      return res.status(500).json({ error })
+    }
+    if (!company) return res
+        .status(404)
+        .json({ success: false, message: 'Specified company was not found' })
+    return Zone.findOne({ name: zone, company: company._id }).exec(
+      (error, zone) => {
+        if (error) {
+          winston.error(error)
+          return res.status(500).json({ error })
+        }
+        if (!zone) return res
+            .status(404)
+            .json({ success: false, message: 'Specified zone was not found' })
+        return Site.findOne({ key, company: company._id }).exec(
+          (error, site) => {
+            if (error) {
+              winston.error(error)
+              return res.status(500).json({
+                success: false,
+                message:
+                  'Could not add the smartbox to the already created site',
+                error
+              })
+            }
+            new Stream({
+              core,
+              user,
+              pass,
+              streamid,
+              version,
+              company,
+              key,
+              name,
+              country,
+              zone
+            }).save()
+            const token = jwt.sign({
+              "exp": Math.floor(Date.now() / 1000) + (60 * 60),
+              "iat": Date.now()
+            },base64url('mefcwbUTxYZHLa_EalRisajyFZD8dCLHYkcBQ1mWuiA'))
+            return res.status(200).json({ site })
+          })
+      })
+  })
 })
 
 module.exports = router
