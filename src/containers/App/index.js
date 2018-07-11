48fb33dc0ef7e105f38844c502d9eaadaf70a4f3
Jacob García <rock2494@hotmail.com>
2018-07-11 18:42:44 -0500

    Changed socket to base window

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -171,7 +171,7 @@ class App extends Component {
   }
 
   initSockets() {
-    this.socket = io('https://att.connus.mx')
+    this.socket = io()
 
     this.socket.on('connect', () => {
       this.socket.emit('join', 'connus')
