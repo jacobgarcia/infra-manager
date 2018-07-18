5231fb795744e88905906eafa5c4344cdc5657cc
Jacob García <rock2494@hotmail.com>
2018-07-18 16:34:58 -0500

    Fix: Can't Set Headers After They Are Sent

--- router/v1/reports.js
+++ router/v1/reports.js
@@ -33,11 +33,7 @@ router.route('/reports/alarms').get((req, res) => {
       return res.sendFile(`${process.env.PWD}/static/alarms.csv`, error => {
         if (error) {
           winston.error({ error })
-          return res.status(500).json({ error })
         }
-        return res
-          .status(200)
-          .json({ success: true, message: 'Sent file to client' })
       })
     })
 })
