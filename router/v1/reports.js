a2504f79ede3d7e0ae76ee34eb3671d952d3cd47
Jacob García <rock2494@hotmail.com>
2018-07-20 15:11:08 -0500

    Log alarms

--- router/v1/reports.js
+++ router/v1/reports.js
@@ -25,6 +25,7 @@ router.route('/reports/alarms').get((req, res) => {
         site.alarms.map(alarm => {
           alarm.site = site.name
           alarm.zone = site.zone.name
+          console.log(alarm)
           alarms.push(alarm)
         })
       })
@@ -33,7 +34,7 @@ router.route('/reports/alarms').get((req, res) => {
       return res.status(200).json({
         success: true,
         message: 'Successfully generated report',
-        sites
+        alarms
       })
     })
 })
