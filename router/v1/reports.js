42e27e5eacad8fd87329a283143a864577a7c03d
Jacob García <rock2494@hotmail.com>
2018-07-20 16:00:38 -0500

    WORKING. DON'T YOU?

--- router/v1/reports.js
+++ router/v1/reports.js
@@ -38,7 +38,9 @@ router.route('/reports/alarms').get((req, res) => {
         })
       })
 
-      Site.csvReadStream(alarms).pipe(fs.createWriteStream('static/alarms.csv'))
+      Site.csvReadStream(moreAlarms).pipe(
+        fs.createWriteStream('static/alarms.csv')
+      )
       return res.status(200).json({
         success: true,
         message: 'Successfully generated report',
