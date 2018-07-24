aeed9d00e8a948520be80d4c968f84d8f9becfd5
Jacob García <rock2494@hotmail.com>
2018-07-24 16:52:16 -0500

    ES6 Cron

--- daily_cron.js
+++ daily_cron.js
@@ -1,17 +1,20 @@
 const request = require('request')
 const winston = require('winston')
 
-
-request.get('https://api.connus.mx/v1/reports/alarms/count/PANA-MA',{
-  'auth': {
-    'bearer': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YjU2NDViYjdjMGM4YzhlNDUzNWRkOGEiLCJhY2MiOjQsImNtcCI6IjViNTYzMjM5ODM2OGU3ZTk1ZjA0YzE1NyIsImlhdCI6MTUzMjQ2MzIyNn0.t1Naqm2G6WM6v_33Crp14wp_gsUp4GiV6xWZPsGtCf8'
+request.get(
+  'https://api.connus.mx/v1/reports/alarms/count/PANA-MA',
+  {
+    auth: {
+      bearer:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YjU2NDViYjdjMGM4YzhlNDUzNWRkOGEiLCJhY2MiOjQsImNtcCI6IjViNTYzMjM5ODM2OGU3ZTk1ZjA0YzE1NyIsImlhdCI6MTUzMjQ2MzIyNn0.t1Naqm2G6WM6v_33Crp14wp_gsUp4GiV6xWZPsGtCf8'
+    }
+  },
+  (err, resp) => {
+    if (err) {
+      winston.error(err)
+    }
+    if (resp.statusCode === 200) {
+      console.log('Daily Update')
+    }
   }
-},
-(err, resp) => {
-  if (err) {
-    winston.error(err)
-  }
-  if (resp.statusCode === 200) {
-    console.log("Daily Update")
-  }
-})
+)
