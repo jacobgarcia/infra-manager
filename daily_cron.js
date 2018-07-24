05dc900750bf6797bd8f73184503a1a2c4965c50
Unknown <franscaraveli@gmail.com>
2018-07-24 15:53:42 -0500

    Rearing endpoint daily report

--- daily_cron.js
+++ daily_cron.js
@@ -2,7 +2,7 @@ const request = require('request')
 const winston = require('winston')
 
 
-request.put('https://api.connus.mx/v1//reports/alarms/count/PANA-MA',{
+request.get('https://api.connus.mx/v1/reports/alarms/count/PANA-MA',{
   'auth': {
     'bearer': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YjU2NDViYjdjMGM4YzhlNDUzNWRkOGEiLCJhY2MiOjQsImNtcCI6IjViNTYzMjM5ODM2OGU3ZTk1ZjA0YzE1NyIsImlhdCI6MTUzMjQ2MzIyNn0.t1Naqm2G6WM6v_33Crp14wp_gsUp4GiV6xWZPsGtCf8'
   }
@@ -12,6 +12,6 @@ request.put('https://api.connus.mx/v1//reports/alarms/count/PANA-MA',{
     winston.error(err)
   }
   if (resp.statusCode === 200) {
-    console.log("Hourly Only Update")
+    console.log("Daily Update")
   }
 })
