9e5b76f5a9c6b9be59f7e24ff553eca51a6b6707
Jacob García <rock2494@hotmail.com>
2018-07-24 16:59:03 -0500

    ES6 Hourly Cron

--- hourly_cron.js
+++ hourly_cron.js
@@ -1,17 +1,20 @@
 const request = require('request')
 const winston = require('winston')
 
-
-request.put('https://api.connus.mx/v1/sites/online',{
-  'auth': {
-    'bearer': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTg0ODMwMjQ3ZjBhZTEwZTkwMDNkZDciLCJhY2MiOjQsImNtcCI6IjVhMDA3Yzk2YjhhNWU4NDM1ZmQ5MGVhYSIsImlhdCI6MTUzMDY1MDAyNH0.r4qLHckl_Lw7HG3ZHipqm05hOVwLDeZLVjkZUi0mjzI'
+request.put(
+  'https://api.connus.mx/v1/sites/online',
+  {
+    auth: {
+      bearer:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTg0ODMwMjQ3ZjBhZTEwZTkwMDNkZDciLCJhY2MiOjQsImNtcCI6IjVhMDA3Yzk2YjhhNWU4NDM1ZmQ5MGVhYSIsImlhdCI6MTUzMDY1MDAyNH0.r4qLHckl_Lw7HG3ZHipqm05hOVwLDeZLVjkZUi0mjzI'
+    }
+  },
+  (err, resp) => {
+    if (err) {
+      winston.error(err)
+    }
+    if (resp.statusCode === 200) {
+      console.log('Hourly Only Update')
+    }
   }
-},
-(err, resp) => {
-  if (err) {
-    winston.error(err)
-  }
-  if (resp.statusCode === 200) {
-    console.log("Hourly Only Update")
-  }
-})
+)
