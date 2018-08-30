c83f567535740e94e45bc84aed2d7e93e944d588
Jacob García <rock2494@hotmail.com>
2018-08-30 17:22:19 -0500

    DB reference update

--- config/index.js
+++ config/index.js
@@ -6,5 +6,5 @@ module.exports = {
     backgroundColor: '#1d2229'
   },
   secret: 'sUp3-rS.ecr3t{pass}',
-  databaseUri: process.env.MONGODB_URL || 'mongodb://52.72.2.200:27017/kawlantid'
+  databaseUri: process.env.MONGODB_URL || 'mongodb://localhost:27017/kawlantid'
 }
