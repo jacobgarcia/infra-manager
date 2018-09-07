8ffb956c7b2ac6eb775c3fd39f26b299b6187eae
Jacob García <rock2494@hotmail.com>
2018-09-07 17:54:47 -0500

    Changed to localhost

--- config/index.js
+++ config/index.js
@@ -6,5 +6,5 @@ module.exports = {
     backgroundColor: '#1d2229'
   },
   secret: 'sUp3-rS.ecr3t{pass}',
-  databaseUri: process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/kawlantid'
+  databaseUri: process.env.MONGODB_URL || 'mongodb://localhost:27017/kawlantid'
 }
