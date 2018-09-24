c25a5213383a636cc774d7361dbf98280667eb08
Jacob García <rock2494@hotmail.com>
2018-09-24 11:39:46 -0500

    Removed unused collections

--- models/FrmUser.js
+++ models/FrmUser.js
@@ -4,14 +4,14 @@ const Schema = mongoose.Schema
 
 const schema = new Schema({
   privacy: Boolean,
-  photo: {type:String, default: null },// Url
+  photo: { type: String, default: null }, // Url
   pin: { type: String, unique: true, required: true },
   site: { type: String, ref: 'Site' },
   startdate: { type: Number, default: null },
-  enddate: { type: Number, default: null } ,
+  enddate: { type: Number, default: null },
   isLogged: { type: Boolean, default: false }
 })
 
-schema.index({ name: 'text', lastname: 'text', pin: 'text'})
+schema.index({ name: 'text', lastname: 'text', pin: 'text' })
 
 module.exports = mongoose.model('FrmUser', schema)
